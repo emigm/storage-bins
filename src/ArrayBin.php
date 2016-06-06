e7809a8102b4b9100276f3d8e5da3e1a13df95db
Emiliano G. Molina <emiliano.g.molina@gmail.com>
2016-06-06 20:14:10 -0300

    Applies php-cs-fixer changes

--- src/ArrayBin.php
+++ src/ArrayBin.php
@@ -15,7 +15,8 @@ class ArrayBin
         $this->is_camelcase = $is_camelcase;
     }
 
-    public function __call($name, $arguments) {
+    public function __call($name, $arguments)
+    {
         $result = preg_match(self::GETTER_REGEX, $name, $matches);
 
         if (false === $result) {
@@ -34,7 +35,7 @@ class ArrayBin
             $attribute_value = $this->array[$attribute_nane];
 
             return ($this->isAssociativeArray($attribute_value))
-                ? new ArrayBin($attribute_value, $this->is_camelcase)
+                ? new self($attribute_value, $this->is_camelcase)
                 : $attribute_value;
         }
     }
@@ -59,6 +60,6 @@ class ArrayBin
 
     protected function isAssociativeArray($array)
     {
-        return is_array($array) AND array_keys($array) !== range(0, count($array) - 1);
+        return is_array($array) and array_keys($array) !== range(0, count($array) - 1);
     }
 }
